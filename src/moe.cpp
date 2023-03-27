68c96e949711152f26159909568ae701db9aa7eb
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-03-27 19:58:39 +0100

    At least it runs
    But the numbers don't match yet

--- src/moe.cpp
+++ src/moe.cpp
@@ -34,9 +34,32 @@ dnnl::memory::dim product(const dnnl::memory::dims &dims) {
 // Utility templates for printing 2D tensors to ostreams.
 template <typename T>
 struct matrix {
-	size_t w;
-	size_t h;
-	T* data;
+	memory::dim h;
+	memory::dim w;
+	T const *data;
+
+	// Explicit constructor
+	matrix(memory::dim h, memory::dim w, const T *data) :
+		h(h),
+		w(w),
+		data(data) {}
+
+	matrix(memory::dim h, memory::dim w, cnpy::NpyArray const &data) :
+		h(h),
+		w(w),
+		data(data.data<T>()) {
+		assert(data.num_vals == w * h);
+		assert(data.num_bytes() == w * h * sizeof(T));
+	}
+
+	// One that derives from oneDNN
+	explicit matrix(dnnl::memory const &mem) :
+		h(mem.get_desc().get_dims()[0]),
+		w(mem.get_desc().get_dims()[1]),
+		data(reinterpret_cast<T const *>(mem.get_data_handle())) {
+			// Make sure you've executed the command stream until this point in the
+			// code and we're ready to read memory. This isn't done for you.
+	}
 };
 
 template <typename T>
@@ -55,17 +78,27 @@ std::ostream &operator<<(std::ostream &out, number<uint8_t> const &number) {
 	return out << std::fixed << std::setw(4) << static_cast<size_t>(number.val);
 }
 
-template <typename T>
+template <typename T, std::size_t N=4>
 std::ostream &operator<<(std::ostream &out, matrix<T> const &matrix) {
 	// store precision so we can restore it.
 	const auto default_precision(out.precision());
 	const auto default_width(out.width());
 
 	out << std::setprecision(4);
-	out << "(" << matrix.w << "x" << matrix.h << ") " << typeid(T).name() << " {\n";
-	for (size_t x = 0; x < matrix.w; ++x) {
-		for (size_t y = 0; y < matrix.h; ++y) {
-			out << (y > 0 ? "," : " ") << number<T>{matrix.data[x*matrix.h + y]};
+	out << "(" << matrix.h << "x" << matrix.w << ") " << typeid(T).name() << " {\n";
+	for (size_t y = 0; y < matrix.h; ++y) {
+		// Jump forward
+		if (y == N && y+N < matrix.h) {
+			out << "   ...\n";
+			y = matrix.h - N;
+		}
+
+		for (size_t x = 0; x < matrix.w; ++x) {
+			if (x == N && x+N < matrix.w) {
+				out << ", ...";
+				x = matrix.w - N;
+			}
+			out << (x > 0 ? "," : " ") << number<T>{matrix.data[y*matrix.w + x]};
 		}
 		out << "\n";
 	}
@@ -73,6 +106,25 @@ std::ostream &operator<<(std::ostream &out, matrix<T> const &matrix) {
 
 	// Restore previous precision
 	return out << std::setprecision(default_precision) << std::setw(default_width);
+}
+
+// Print a loaded npz (well, names and sizes)
+std::ostream &operator<<(std::ostream &out, cnpy::npz_t const &map) {
+	out << "{\n";
+	for (auto &&entry : map)
+		out << "  " << entry.first << " = <" << entry.second.word_size << "b * " << entry.second.num_vals << ">\n";
+	return out << "}";
+}
+
+// Easy dims printing
+std::ostream &operator<<(std::ostream &out, memory::dims const &dims) {
+	out << "[";
+	for (auto it = dims.begin(); it != dims.end(); ++it) {
+		if (it != dims.begin())
+			out << ", ";
+		out << *it;
+	}
+	return out << "]";
 }
 
 // Very simple replacement for std::format introduced in C++20. Only supports
@@ -101,38 +153,64 @@ std::string format(std::string const &formatTemplate, Arg arg, Args... args) {
 // Memory structure for an Expert, so we can have std::vector<Expert> instead of
 // a bunch of vectors. Efficiency was not on my mind when deciding this.
 struct Expert {
-	std::size_t size;
-	std::vector<float> weights;
-	std::vector<float> bias;
-	
-	Expert(std::size_t size, const float *w_data, const float *b_data) :
-		size(size),
-		weights(w_data, w_data + size * size),
-		bias(b_data, b_data + size)
-	{
-		//
-	}
+	std::vector<float> w1;
+	std::vector<float> b1;
+	std::vector<float> w2;
+	std::vector<float> b2;
 };
+
+// Get scalar from our loaded `data` npz file
+template <typename T>
+T scalar(cnpy::NpyArray const &arr) {
+	assert(arr.num_vals == 1);
+	return arr.data<T>()[0];
+}
+
+// Little macro to give me the i'th slice of a bigger chunk of data. Think of
+// the difference between calloc and malloc: does the same thing, but
+// communicates different intention.
+template <typename T>
+std::vector<T> take_slice(std::size_t i, T const *data, const dnnl::memory::dims &dims) {
+	auto size = product(dims);
+	return std::vector<T>(data + i * size, data + (i + 1) * size);
+}
 
 int main(int argc, char** argv) {
 	assert(argc > 1);
 	auto data = cnpy::npz_load(argv[1]);
+
+	std::cout << "Loaded npz from " << argv[1] << " " << data << std::endl; 
 
 	dnnl::engine engine(dnnl::engine::kind::cpu, 0);
 
 	dnnl::stream engine_stream(engine);
 
 	// Tensor dimensions.
-	const memory::dim expert_count = 3, token_count = 7, embedding_size = 5;
+	const memory::dim expert_count   = scalar<long>(data["expert_count"]),
+	                  expert_size    = scalar<long>(data["expert_size"]),
+	                  embedding_size = scalar<long>(data["embedding_size"]),
+	                  token_count    = scalar<long>(data["token_count"]);
 
-	float b = 0.1; // bypass multiplier
+	// bypass multiplier
+	float b = scalar<float>(data["b"]);
+
+	std::cout << "parameters { \n"
+	          << "  expert_count   = " << expert_count << "\n"
+	          << "  expert_size    = " << expert_size << "\n"
+	          << "  embedding_size = " << embedding_size << "\n"
+	          << "  token_count    = " << token_count << "\n"
+	          << "  b              = " << b << "\n"
+	          << "}\n";
 
 	// Source (src), weights, bias, and destination (dst) tensors dimensions.
 	memory::dims src_dims = {token_count, embedding_size};
 	memory::dims dst_dims = {token_count, embedding_size};
 
-	memory::dims expert_w_dims = {embedding_size, embedding_size};
-	memory::dims expert_b_dims = {1, embedding_size};
+	memory::dims expert_w1_dims = {embedding_size, expert_size};
+	memory::dims expert_b1_dims = {1, expert_size};
+
+	memory::dims expert_w2_dims = {expert_size, embedding_size};
+	memory::dims expert_b2_dims = {1, embedding_size};
 
 	// Initialize some source data
 	assert(product(src_dims) == data["src"].num_vals);
@@ -152,11 +230,12 @@ int main(int argc, char** argv) {
 	std::vector<Expert> experts;
 
 	for (size_t i = 0; i < expert_count; ++i) {
-		experts.emplace_back(
-			embedding_size,
-			data["experts_w"].data<float>() + i * embedding_size * embedding_size,
-			data["experts_b"].data<float>() + i * embedding_size
-		);
+		experts.emplace_back(Expert{
+			take_slice(i, data["experts_w1"].data<float>(), expert_w1_dims),
+			take_slice(i, data["experts_b1"].data<float>(), expert_b1_dims),
+			take_slice(i, data["experts_w2"].data<float>(), expert_w2_dims),
+			take_slice(i, data["experts_b2"].data<float>(), expert_b2_dims)
+		});
 	}
 
 	// ... and so it begins ...
@@ -191,11 +270,6 @@ int main(int argc, char** argv) {
 	std::unordered_map<int, memory> concat_args;
 #endif
 
-#ifdef CONCAT_INDEX_RESTORE
-	std::vector<memory::desc> expert_dst_descs;
-	std::vector<memory> expert_dst_mems;
-#endif
-
 	for (size_t i = 0; i < expert_count; ++i) {
 #ifdef CONCAT_INDEX_SELECT
 		// === oneDNN call to concat() to select rows for expert ===
@@ -215,6 +289,8 @@ int main(int argc, char** argv) {
 
 		memory::dim expert_token_count = concat_src_mems.size();
 
+		std::cout << "Expert " << i << " gets " << expert_token_count << " tokens\n";
+
 		// Create concat primitive descriptor.
 		auto concat_desc = concat::primitive_desc(engine, 0, concat_src_desc);
 		auto concat_prim = concat(concat_desc);
@@ -227,7 +303,7 @@ int main(int argc, char** argv) {
 
 		engine_stream.wait(); // Purely for debugging
 #else
-		// === Naive implementation of concat ===
+		// === Naive implementation of concat to select rows for expert ===
 		memory::dim expert_token_count = std::accumulate(&router[token_count * i], &router[token_count * (i + 1)], 0, [](uint8_t acc, uint8_t val) {
 			return acc + (val ? 1 : 0);
 		});
@@ -248,99 +324,95 @@ int main(int argc, char** argv) {
 		}
 #endif
 
-		std::cout << "expert_" << i << "_src_mem = " << matrix<float>{
-			static_cast<size_t>(expert_token_count), embedding_size,
-			reinterpret_cast<float*>(expert_src_mem.get_data_handle())};
+		std::cout << "expert_" << i << "_src_mem = " << matrix<float>(expert_src_mem);
+		std::cout << "expected expert_" << i << "_src_mem = " << matrix<float>(expert_token_count, embedding_size, data[format("expert_{}_src", i)]);
 
-#ifdef CONCAT_INDEX_RESTORE
-		auto expert_dst_desc = expert_dst_descs.emplace_back(memory::dims{expert_token_count, embedding_size}, dt::f32, tag::ab);
-		auto expert_dst_mem = expert_dst_mems.emplace_back(expert_dst_descs.back(), engine);
-#else
+		// Temporary memory used for expert's intermediate result
+		auto expert_tmp_desc = memory::desc({expert_token_count, expert_size}, dt::f32, tag::ab);
+		auto expert_tmp_mem = memory(expert_tmp_desc, engine);
+
+		// Expert output memory, to be selectively added to dst_mem eventually
 		auto expert_dst_desc = memory::desc({expert_token_count, embedding_size}, dt::f32, tag::ab);
 		auto expert_dst_mem = memory(expert_dst_desc, engine);
-#endif
 
-		auto expert_weights_desc = memory::desc(expert_w_dims, dt::f32, tag::ab);
-		auto expert_weights_mem = memory(expert_weights_desc, engine, experts[i].weights.data());
+		// Expert weights and biases (these are all the same shape for every expert)
+		auto expert_w1_desc = memory::desc(expert_w1_dims, dt::f32, tag::ab);
+		auto expert_w1_mem = memory(expert_w1_desc, engine, experts[i].w1.data());
 		
-		auto expert_bias_desc = memory::desc(expert_b_dims, dt::f32, tag::ab);
-		auto expert_bias_mem = memory(expert_bias_desc, engine, experts[i].bias.data());
+		auto expert_b1_desc = memory::desc(expert_b1_dims, dt::f32, tag::ab);
+		auto expert_b1_mem = memory(expert_b1_desc, engine, experts[i].b1.data());
+
+		auto expert_w2_desc = memory::desc(expert_w2_dims, dt::f32, tag::ab);
+		auto expert_w2_mem = memory(expert_w2_desc, engine, experts[i].w2.data());
+		
+		auto expert_b2_desc = memory::desc(expert_b2_dims, dt::f32, tag::ab);
+		auto expert_b2_mem = memory(expert_b2_desc, engine, experts[i].b2.data());
 
 		// Create primitive descriptor.
-		auto matmul_desc = matmul::primitive_desc(engine,
+		auto matmul_m1_desc = matmul::primitive_desc(engine,
 			expert_src_desc,
-			expert_weights_desc,
-			expert_bias_desc,
+			expert_w1_desc,
+			expert_b1_desc,
+			expert_tmp_desc);
+
+		// Create the primitive.
+		auto matmul_m1_prim = matmul(matmul_m1_desc);
+
+		// Primitive execution: matrix multiplication
+		matmul_m1_prim.execute(engine_stream, {
+			{DNNL_ARG_SRC,     expert_src_mem},
+			{DNNL_ARG_WEIGHTS, expert_w1_mem},
+			{DNNL_ARG_BIAS,    expert_b1_mem},
+			{DNNL_ARG_DST,     expert_tmp_mem},
+		});
+
+		// Create primitive descriptor.
+		auto matmul_m2_desc = matmul::primitive_desc(engine,
+			expert_tmp_desc,
+			expert_w2_desc,
+			expert_b2_desc,
 			expert_dst_desc);
 
 		// Create the primitive.
-		auto matmul_prim = matmul(matmul_desc);
+		auto matmul_m2_prim = matmul(matmul_m2_desc);
 
-		// Primitive arguments.
-		std::unordered_map<int, memory> matmul_args;
-		matmul_args.insert({DNNL_ARG_SRC,     expert_src_mem});
-		matmul_args.insert({DNNL_ARG_WEIGHTS, expert_weights_mem});
-		matmul_args.insert({DNNL_ARG_BIAS,    expert_bias_mem});
-		matmul_args.insert({DNNL_ARG_DST,     expert_dst_mem});
+		matmul_m2_prim.execute(engine_stream, {
+			{DNNL_ARG_SRC,     expert_tmp_mem},
+			{DNNL_ARG_WEIGHTS, expert_w2_mem},
+			{DNNL_ARG_BIAS,    expert_b2_mem},
+			{DNNL_ARG_DST,     expert_dst_mem},
+		});
 
-		// Primitive execution: matrix multiplication
-		matmul_prim.execute(engine_stream, matmul_args);
+		// std::cout << "expert_" << i << "_weights = " << matrix<float>(expert_weights_mem);
+		std::cout << "expert_" << i << "_dst = " << matrix<float>(expert_dst_mem);
+		std::cout << "expected expert_" << i << "_dst_mem = " << matrix<float>(expert_token_count, embedding_size, data[format("expert_{}_dst", i)]);
 
-		// std::cout << "expert_" << i << "_weights = " << matrix<float>{embedding_size,embedding_size,reinterpret_cast<float*>(expert_weights_mem.get_data_handle())};
-		std::cout << "expert_" << i << "_dst = " << matrix<float>{static_cast<size_t>(expert_token_count), embedding_size, reinterpret_cast<float*>(expert_dst_mem.get_data_handle())};
-		std::cout << "expected expert_" << i << "_dst_mem = " << matrix<float>{static_cast<size_t>(expert_token_count), embedding_size, data[format("expert_{}_dst", i)].data<float>()};
-		
-#ifndef CONCAT_INDEX_RESTORE
-		// Copy results from expert back to final output memory
-		engine_stream.wait();
-
+		// Copy data back to the final output. Since we're doing Top-K and we want
+		// to sum the expert of multiple experts per token, we're just going to
+		// for-loop binary add for each row. I'm open to better implementations!
 		for (auto j = 0, dst_offset = 0; j < expert_token_count; ++j, ++dst_offset) {
 			// Find the next True in the router to figure out the offset in dst_mem
 			while (!router[token_count * i + dst_offset])
 				++dst_offset;
 
-			std::memcpy(
-				reinterpret_cast<float*>(dst_mem.get_data_handle()) + (embedding_size * dst_offset),
-				reinterpret_cast<float*>(expert_dst_mem.get_data_handle()) + (embedding_size * j),
-				embedding_size * sizeof(float));
-		}
+			auto expert_slice_desc = expert_dst_desc.submemory_desc({1, embedding_size}, {j, 0});
 
-		engine_stream.wait(); // for debugging only
-		std::cout << "dst_data (after expert " << i << ") = " << matrix<float>{token_count, embedding_size, reinterpret_cast<float*>(dst_mem.get_data_handle())};
-#endif
-	}
+			auto dst_slice_desc = dst_desc.submemory_desc({1, embedding_size}, {dst_offset, 0});
+			
+			auto binary_desc = binary::primitive_desc(engine, algorithm::binary_add,
+				expert_slice_desc, // source 0
+				dst_slice_desc,    // source 1
+				dst_slice_desc);   // dest
 
-#ifdef CONCAT_INDEX_RESTORE
-	// Silly way of restoring the final output matrix by selectively concatinating
-	// from the various expert output matrices. I don't see how this can scale to
-	// a large number of experts.
-	std::unordered_map<int, memory> dst_concat_args;
-	std::vector<memory::desc> dst_concat_descs;
-	std::vector<memory> dst_concat_mems;
+			auto binary_prim = binary(binary_desc);
 
-	// Offsets which row inside each expert we're at
-	std::vector<memory::dim> expert_src_offsets(expert_count, 0);
-
-	for (auto j = 0; j < token_count; ++j) {
-		for (auto i = 0; i < expert_count; ++i) {
-			// Find the expert that is responsible for this token j.
-			if (!router[token_count * i + j])
-				continue;
-
-			// std::cerr << "For token " << j << " we take row " << expert_src_offsets[i] << " from expert " << i << std::endl;
-			dst_concat_descs.emplace_back(expert_dst_descs[i].submemory_desc({1, embedding_size}, {expert_src_offsets[i]++, 0}));
-			dst_concat_mems.emplace_back(dst_concat_descs.back(), engine, expert_dst_mems[i].get_data_handle());
-			dst_concat_args.insert({DNNL_ARG_MULTIPLE_SRC + j, dst_concat_mems.back()});
-			break; // Go to next token
+			binary_prim.execute(engine_stream, {
+				{DNNL_ARG_SRC_0, expert_dst_mem},
+				{DNNL_ARG_SRC_1, dst_mem},
+				{DNNL_ARG_DST, dst_mem}
+			});
 		}
 	}
-
-	auto dst_concat_desc = concat::primitive_desc(engine, 0, dst_concat_descs);
-	auto dst_concat_prim = concat(dst_concat_desc);
-
-	dst_concat_args.insert({DNNL_ARG_DST, dst_mem});
-	dst_concat_prim.execute(engine_stream, dst_concat_args);
-#endif
 
 	// Wait for the computation to finalize.
 	engine_stream.wait();
